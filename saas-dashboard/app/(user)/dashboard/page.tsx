bea6642e47886176c92adea8be11d95d39da87c2
markbuckle <markbuckle92@gmail.com>
2024-08-13 21:12:47 -0230

    Payments

--- saas-dashboard/app/(user)/dashboard/page.tsx
+++ saas-dashboard/app/(user)/dashboard/page.tsx
@@ -4,6 +4,8 @@ import { projects } from "@/db/schema";
 import { eq } from "drizzle-orm";
 import { auth } from "@clerk/nextjs/server";
 import ProjectsList from "./projects-list";
+import { getSubscription } from "@/actions/userSubscriptions";
+import { maxFreeProjects } from "@/lib/payments";
 
 export default async function Page() {
   const { userId } = auth();
@@ -16,13 +18,18 @@ export default async function Page() {
     .from(projects)
     .where(eq(projects.userId, userId));
 
+  const subscribed = await getSubscription({ userId });
+
   return (
     <div>
       <div className="flex items-center justify-center gap-3">
         <h1 className="text-3xl font-bold text-center my-4">Your Projects</h1>
-        <NewProjBtn />
+        {/* if not subscribed, user can only use the maxFreeProjects amount (plus button disappears) */}
+        {subscribed !== true && userProjects.length > maxFreeProjects ? null : (
+          <NewProjBtn />
+        )}
       </div>
-      <ProjectsList projects={userProjects} />
+      <ProjectsList projects={userProjects} subscribed={subscribed} />
     </div>
   );
 }
---
+++ saas-dashboard/lib/payments.ts
@@ -0,0 +1,5 @@
+// copy and paste the price IDs from the ... line of the prices in the Stripe dashboard
+export const monthlyPlanId: string="price_1PnSa301FeyjNCENfVoowWNq";
+export const yearlyPlanId: string="price_1PnSZB01FeyjNCENYqsVITmG";
+export const maxFreeProjects: number = 3;
+
